0477ae839c2c86dceaa2a1d6443783f3790f823f
MacBook <macbook@MacBooks-MacBook-Pro.local>
2020-08-17 11:38:01 +0800

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        emma: '#455e4e',
+      },
+      // Custom
+      // height: (theme) => ({
+      //   full: '100%',
+      //   mini: '50vh',
+      // }),
+    },
+  },
+  variants: {},
+  plugins: [],
+};
